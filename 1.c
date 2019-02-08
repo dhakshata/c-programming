c90eae72555ee76e8a7dee7a8544d33d409ae7cf
dhakshata <36620457+dhakshata@users.noreply.github.com>
2019-02-08 16:22:16 +0530

    Create 1.c

---
+++ 1.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+ 
+int main(void) {
+	int a;
+ 
+	scanf("%d",&a);
+ 
+if(a=0)
+printf("ZERO");
+ 
+else if(a>0)
+printf("POSITIVE");
+ 
+else
+printf("NEGATIVE");
+ 
+	return 0;
+}
