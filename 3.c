d20873c087078fcb06e9437bf91b755efdaf811a
dhakshata <36620457+dhakshata@users.noreply.github.com>
2019-02-08 18:35:59 +0530

    Create 3.c

---
+++ 3.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include<stdio.h>
+ 
+int main() {
+	char a;
+	char str[100]={'a','e','i','o','u','\0'};
+	scanf("%s",c);
+	if(str=='a'||str=='e'||str=='i'||str=='o'||str=='u')
+	printf("Vowel");
+	else
+	printf("Consonant");
+	return 0;
+}
